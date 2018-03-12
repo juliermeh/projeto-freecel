38bf59f33d5abe859af74ef8d3e8a8ccc3b936fe
Juvenal <juvenalfisica@live.com>
2018-03-12 12:59:48 -0300

    tradução do arquivo pilha.h

--- Pilha.h
+++ Pilha.h
@@ -28,52 +28,55 @@
 
 typedef struct ItemList
 {
-    int value; //No futuro, achar um método de transformar este tipo em algo mais dinâmico
-    struct ItemList *next;
+    int valor; //No futuro, achar um método de transformar este tipo em algo mais dinâmico
+    struct ItemList *proximo;
 }Item;
 
-typedef struct PileStructure
+typedef struct PilhaStructure
 {
     Item *start;
     int length; // length ao inves de size, pois size ja é definido para outras coisas
-}Pile;
+}Pilha;
 
 
 // Pile* createPile();
 // Retorna uma NOVA pilha inicializada
 // Retorna NULL caso não consiga criar uma nova pilha
-Pile* createPile();
+
+Pilha* crearPilha();
 
 
 // int addItemPile(Pile* pile, int value)
 // Cria e adiciona um novo item à pilha, com valor value
 // Retorna -1 se não for bem sucedido, 0 se for bem sucedido
-int addNewItemPile(Pile* pile, int value);
-int addItemPile(Pile* pile, Item* item);
+
+int adicionarNovoItemPilha(Pilha* pilha, int valor);
+int adicionarItemPilha(Pilha* pilha, Item* item);
 
 // Item* removeTopItemPile(Pile* pile)
 // remove o primeiro item da pilha
 // retorna NULL se houver algum erro, ou o item removido para uso futuro
 // O "next" do item removido vira NULL, pois ele é removido da pilha.
 // Nota: o item NÃO É DELETADO DA MEMÓRIA.
-Item* removeTopItemPile(Pile* pile);
+Item* removeTopItemPilha(Pilha* pilha);
 
 // int deleteItem(Item* item)
 // deleta e remove o item da memória
 // retorna -1 se o next não for NULL, pois siginifica que ainda está em uma pilha
 // retorna 0 se for bem sucedido
+
 int deleteItem(Item* item);
 
 // int deletePile(Pile* pile)
 // deleta a pilha e remove da memória
 // remove e deleta qualquer item que esteja na pilha
 // retorna -1 se a pilha não existir
-int deletePile(Pile* pile);
+int deletePilha(Pilha* pilha);
 
 //Item* getPreviousItemOnPile(Item *item, Pile *pile)
 // retorna o item anterior ao item requerido na pilha
 // retorna o próprio item se ele for o topo da pilha
 // retorna NULL se não for possível, ou se o item não estiver na pilha
-Item* getPreviousItemOnPile(Item *item, Pile *pile);
+Item* getPreviousItemOnPilha(Item *item, Pilha *pilha);
 
 #endif // PILHA_H_INCLUDED
